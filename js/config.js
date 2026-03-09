b48e3b2622a307278bec5cb2c0aaed15169d61ec
chaoyuchieh <cyc9e9@gmail.com>
2026-03-09 17:44:17 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     SUPABASE_URL: 'https://gjuhnggtywwuziqtymmg.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqdWhuZ2d0eXd3dXppcXR5bW1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMDU1NTMsImV4cCI6MjA4Mjg4MTU1M30.3Wv8jfSjYBZ0BMKWn4jh1wEkDuYgiB0j-mXCOpADmQc',
-    LIFF_ID: '2008856015-4bLaPE5n',
+    LIFF_ID: '2009380924-KARfpWqM',
     LINE_MESSAGE_API_URL: '/api/send-line-message',
     LINE_OFFICIAL_ID: '@ydz8706m',
     ADMIN_LINE_USER_ID: 'U57241a2c907fec7fe18196e1fd4d10ea',
